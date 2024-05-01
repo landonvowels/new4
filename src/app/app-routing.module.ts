c48f2bc47c9aede7bf651d60e61a67162991136f
Landon Vowels <ldv8df@umsystem.edu>
2024-05-01 01:17:06 -0500

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostListComponent } from './post/post-list/post-list.component';
+import { PostComponent } from './posts/post-create/post/post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', component: PostListComponent
+    },
+  {
+  path:'projects', component: PostListComponent
+  },
+  {
+  path:'create',component: PostComponent
+  }
+  ]
+  
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
