ee8e58d4a6b20ba7c485e51d036f39163d3a6513
Donovan H <>
2024-05-01 01:29:31 -0500

    donovanbranch

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -1,8 +1,8 @@
+// post.service.ts
 import { Injectable } from '@angular/core';
 import { Post } from './post.model';
 import { BehaviorSubject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +11,10 @@ export class PostService {
   private posts: Post[] = [];
   private postsUpdated = new BehaviorSubject<Post[]>([]);
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  // used by post-list component to grab post from server
   fetchServerPost() {
-    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
+    this.http.get<{ message: string, posts: Post[] }>('http://localhost:3000/api/posts')
       .subscribe((postData) => {
         this.posts = postData.posts;
         this.postsUpdated.next([...this.posts]);
@@ -24,17 +23,12 @@ export class PostService {
       });
   }
 
-  // Returns an observable for components to subscribe to posts.
-  // used by post-list component
   getPosts() {
     return this.postsUpdated.asObservable();
   }
 
-  // used by post component, when button pressed
   addPost(post: Post) {
     this.posts.push(post);
-    // here is use of spread operator to update the copy of array
     this.postsUpdated.next([...this.posts]);
   }
-
 }
