c56de97ad906936fd58545529c638b3ac9d4fce2
Landon Vowels <ldv8df@umsystem.edu>
2024-05-01 23:31:39 -0500

    add recent post

---
+++ src/app/recent-posts/recent-posts.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Post } from './../posts/post.model';
+import { PostService } from './../posts/post.service';
+
+
+@Component({
+  selector: 'app-recent-posts',
+  templateUrl: './recent-posts.component.html',
+  styleUrl: './recent-posts.component.css'
+})
+
+export class RecentPostsComponent implements OnInit {
+  posts: Post[] = [];
+  private postsSub: Subscription;
+
+  constructor(private postService: PostService) {}
+
+  ngOnInit() {
+    this.postService.fetchRecentPostsFromServer();
+    this.postsSub = this.postService.getPosts().subscribe((posts: Post[]) => {
+      this.posts = posts;
+    });
+  }
+
+  // Function to delete a post
+  deletePost(postId: string) {
+    this.postService.deletePost(postId);
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
+}
