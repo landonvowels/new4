5e8281525c875b5ec8691fe20e412530f224fb49
Landon Vowels <ldv8df@umsystem.edu>
2024-05-01 23:30:50 -0500

    add recent post dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PostListComponent } from './post/post-list/post-list.component';
 import { PostComponent } from './posts/post-create/post/post.component';
+import { RecentPostsComponent } from './recent-posts/recent-posts.component';
 
 const routes: Routes = [
   {
-    path:'', component: PostListComponent
+    path:'', component: RecentPostsComponent
     },
   {
   path:'projects', component: PostListComponent
