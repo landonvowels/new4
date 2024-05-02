2f6b2b46e54bbdba8276eae324160a7ff9a8b68d
Landon Vowels <ldv8df@umsystem.edu>
2024-05-02 01:43:52 -0500

    add tasks

--- backend/app.js
+++ backend/app.js
@@ -55,7 +55,8 @@ app.post('/api/posts', (req, res, next) => {
   const post = new Post({
     title: req.body.title,
     content: req.body.content,
-    date: req.body.date
+    date: req.body.date,
+    tasks: req.body.tasks
   });
   post.save()
     .then(createdPost => {
@@ -66,6 +67,53 @@ app.post('/api/posts', (req, res, next) => {
     })
     .catch(error => {
       res.status(500).json({ message: 'Failed to add post', error: error });
+    });
+});
+
+app.post('/api/posts/:id/tasks', (req, res, next) => {
+  const { postId } = req.params;
+  const task = req.body;
+  
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        return res.status(404).json({ message: 'Post not found' });
+      }
+
+      post.tasks.push(task);
+      return post.save();
+    })
+    .then(updatedPost => {
+      res.status(201).json({ message: 'Task added', project: updatedPost });
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'Adding task failed', error });
+    });
+});
+
+app.patch('/api/posts/:postId/tasks/:taskId', (req, res) => {
+  const { postId, taskId } = req.params;
+  const { completed } = req.body;
+
+  Post.findById(postId)
+    .then(post => {
+      if (!post) {
+        return res.status(404).json({ message: 'Post not found' });
+      }
+
+      const task = post.tasks.id(taskId);
+      if (!task) {
+        return res.status(404).json({ message: 'Task not found' });
+      }
+
+      task.completed = completed;
+      return post.save();
+    })
+    .then(updatedPost => {
+      res.status(200).json({ message: 'Task updated', post: updatedPost });
+    })
+    .catch(error => {
+      res.status(500).json({ message: 'Updating task failed', error });
     });
 });
 
--- backend/post.model.js
+++ backend/post.model.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
 
+
+const taskSchema = mongoose.Schema({
+    description: { type: String, required: true },
+    completed: { type: Boolean, default: false }
+  });
+
 const postSchema = mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
-  date: { type: Date, required: true}
+  date: { type: Date, required: true},
+  tasks: { type: [taskSchema], required: false}
 });
 
 module.exports = mongoose.model('Post', postSchema, 'posts');
